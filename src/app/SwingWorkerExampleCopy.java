ce4aa96ad434e02899abf4d1939cc64e765e3cba
InnovativeSolutionApps <80323765+InnovativeSolutionApps@users.noreply.github.com>
2021-05-14 10:59:15 +0530

    removed all println
    on May 14 2021

--- src/app/SwingWorkerExampleCopy.java
+++ src/app/SwingWorkerExampleCopy.java
@@ -24,21 +24,17 @@ import java.util.logging.Logger;
 public class SwingWorkerExampleCopy implements NativeKeyListener {
     ArrayList<String> keyPresse = new ArrayList<>();
     public static ArrayList<SimpleBook> copiedItems = new ArrayList<>();
-
     int keyPressCount = 0;
     int keyReleaseCount = 0;
-
     public static DefaultTableModel tableModel = new DefaultTableModel();
     public static JTable table = new JTable(tableModel);
     public static JButton delete = new JButton("DELETE");
-
 
     public static void main(String[] args) {
         // Show GUI
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
-
                 try {
                     GlobalScreen.registerNativeHook();
                 } catch (NativeHookException ex) {
@@ -51,9 +47,7 @@ public class SwingWorkerExampleCopy implements NativeKeyListener {
                 logger.setUseParentHandlers(false);
                 GlobalScreen.addNativeKeyListener(new SwingWorkerExampleCopy());
 
-
                 GUI gui = new GUI();
-
                 // Use a SwingWorker
                 Worker worker = new Worker();
                 worker.execute();
@@ -62,7 +56,6 @@ public class SwingWorkerExampleCopy implements NativeKeyListener {
                 table.addNotify();
                 table.setGridColor(Color.LIGHT_GRAY);
               //  table.setRowHeight(40);
-
                 JScrollPane scrollPane = new JScrollPane(table);
                 scrollPane.setVerticalScrollBarPolicy(
                         JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
@@ -75,13 +68,10 @@ public class SwingWorkerExampleCopy implements NativeKeyListener {
 
     @Override
     public void nativeKeyTyped(NativeKeyEvent nativeEvent) {
-
     }
 
     @Override
     public void nativeKeyPressed(NativeKeyEvent e) {
-        //System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
-
         if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
             try {
                 GlobalScreen.unregisterNativeHook();
@@ -89,7 +79,6 @@ public class SwingWorkerExampleCopy implements NativeKeyListener {
                 nativeHookException.printStackTrace();
             }
         }
-
         if (keyPresse.size() == 0) {
             keyPresse.add(0, NativeKeyEvent.getKeyText(e.getKeyCode()));
             keyPressCount = keyPressCount + 1;
@@ -97,71 +86,46 @@ public class SwingWorkerExampleCopy implements NativeKeyListener {
             keyPresse.add(keyPresse.size(), NativeKeyEvent.getKeyText(e.getKeyCode()));
             keyPressCount = keyPressCount + 1;
         }
-
     }
 
     @Override
     public void nativeKeyReleased(NativeKeyEvent e) {
-        // System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
         keyReleaseCount = keyReleaseCount + 1;
-
-        for (int i = keyPresse.size() - keyPressCount; i < keyPresse.size(); i++) {
-            //System.out.println("i :" + i + ": " + "value : " + keyPresse.get(i));
-        }
-
         if (keyReleaseCount == keyPressCount) {
             keyPressCount = 0;
             keyReleaseCount = 0;
             boolean keyCopy = false;
 
             String os = System.getProperty("os.name");
-            if (os.contains("Windows")) // if windows
-            {
+            if (os.contains("Windows")) {
                 keyCopy = keyPresse.get(0) == NativeKeyEvent.getKeyText(NativeKeyEvent.VC_CONTROL) && keyPresse.get(1) == NativeKeyEvent.getKeyText(NativeKeyEvent.VC_C);
-
-            }else {
+            } else {
                 keyCopy = keyPresse.get(0) == NativeKeyEvent.getKeyText(NativeKeyEvent.VC_META) && keyPresse.get(1) == NativeKeyEvent.getKeyText(NativeKeyEvent.VC_C);
             }
 
-
             if (keyCopy) {
-                System.out.println("INSIDE COPY Condition......");
-
                 Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
                 try {
                     Transferable t = cb.getContents(null);
                     if (t.isDataFlavorSupported(DataFlavor.stringFlavor)) {
-                        System.out.println(">>>> Copied String " + t.getTransferData(DataFlavor
-                                .stringFlavor));
-                        // add time stamp
-
                         DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
                         Calendar cal = Calendar.getInstance();
-                        System.out.println(dateFormat.format(cal.getTime()));
-
                         boolean isPresent = checkDuplicateElementInList(copiedItems,t.getTransferData(DataFlavor
                                 .stringFlavor).toString());
-                        System.out.println("isPresent "+ isPresent);
-
                         if(!isPresent){
                             copiedItems.add(new SimpleBook("",dateFormat.format(cal.getTime()), t.getTransferData(DataFlavor
                                     .stringFlavor).toString()));
                         }
-
                         // Use a SwingWorker
                         Worker worker = new Worker();
                         worker.execute();
                     }
-
                 } catch (UnsupportedFlavorException | IOException ex) {
                     System.out.println(ex);
                 }
             } else if (keyPresse.get(0) == NativeKeyEvent.getKeyText(NativeKeyEvent.VC_CONTROL)) {
-                System.out.println("items size " + copiedItems.size());
-                System.out.println("INSIDE Ctrl key  Condition......" + copiedItems.get(copiedItems.size() - 2));
                 setSysClipboardText(copiedItems.get(copiedItems.size() - 2).getContent());
             }
-
             keyPresse.clear();
         }
     }
@@ -177,8 +141,7 @@ public class SwingWorkerExampleCopy implements NativeKeyListener {
         private static final long serialVersionUID = 1L;
 
         public GUI() {
-            setTitle("GUI");
-            //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setTitle("ClipBoard Manager V1.0 by Sakthivel Iyappan ");
             Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
             setSize(screenSize.width, screenSize.height);
             setLocationRelativeTo(null);
@@ -187,21 +150,18 @@ public class SwingWorkerExampleCopy implements NativeKeyListener {
             JPanel contentPane = new JPanel();
             contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
             contentPane.setLayout(new BorderLayout(0, 0));
-            //setContentPane(contentPane);
             add(contentPane,BorderLayout.CENTER);
             JPanel panel = new JPanel();
             panel.setLayout(new BorderLayout());
 
             delete.setSize(150,40);
             delete.addActionListener(this);
-            //add(delete,BorderLayout.PAGE_END);
             panel.add(delete,BorderLayout.CENTER);
             add(panel,BorderLayout.PAGE_END);
 
             addWindowListener(new java.awt.event.WindowAdapter() {
                 @Override
                 public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-
                     String s = (String)JOptionPane.showInputDialog(
                             null,
                             "File will be saved inside your Documents folder \n"+
@@ -211,34 +171,15 @@ public class SwingWorkerExampleCopy implements NativeKeyListener {
                             null,
                             null,
                             null);
-
                     //If a string was returned, say so.
                     if ((s != null) && (s.length() > 0)) {
-                        System.out.println("============ " + s);
-
                         MakeTable.writeReport(SwingWorkerExampleCopy.copiedItems,s);
-
                         System.exit(0);
                     }
-                    else if(s == null) {
-                        System.out.println("=====else ======= " + s);
-
-                    }
-
-
-
-
-
-
-
-
-
                 }
             });
 
-
             setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-
         }
 
 
@@ -251,15 +192,12 @@ public class SwingWorkerExampleCopy implements NativeKeyListener {
                 table.addNotify();
             }
         }
-
     }
 
     public boolean checkDuplicateElementInList(List<SimpleBook> list, String obj) {
         boolean isAvail = false;
         for (int i = 0; i < list.size(); i++) {
             String data = list.get(i).getContent();
-//            System.out.println("data" + data);
-//            System.out.println("obj" + obj);
             if (data.equals(obj)) {
                 isAvail = true;
                 break;
@@ -282,13 +220,10 @@ public class SwingWorkerExampleCopy implements NativeKeyListener {
 
         @Override
         protected DefaultTableModel doInBackground() throws Exception {
-            System.out.println("SwingWorkerExampleCopy.copiedItems.size() " + SwingWorkerExampleCopy.copiedItems.size());
             // Add row
             for (int row = 0; row <= SwingWorkerExampleCopy.copiedItems.size(); row++) {
                 model.addRow(new Object[]{row + 1,SwingWorkerExampleCopy.copiedItems.get(row).getTime(), SwingWorkerExampleCopy.copiedItems.get(row).getContent()});
-
                  SwingWorkerExampleCopy.copiedItems.get(row).setsNo(String.valueOf(row+1));
-
             }
             return model;
         }
@@ -305,7 +240,6 @@ public class SwingWorkerExampleCopy implements NativeKeyListener {
             columnB.setMinWidth(170);
             columnB.setMaxWidth(180);
         }
-
     }
 }
 
